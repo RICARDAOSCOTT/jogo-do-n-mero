39c44e7324b7af204d6f49e8877993dde2a0be13
RICARDAOSCOTT <163008691+RICARDAOSCOTT@users.noreply.github.com>
2024-10-21 11:32:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
     const campoPalpite = document.querySelector('.campoPalpite');
     let contagemPalpites = 1;
     let botaoReiniciar;
+
+function verificarPalpite() {
+    const palpiteUsuario = Number(campoPalpite.value);
+    if (contagemPalpites === 1) {
+        palpites.textContent = "Palpites anteriores: ";
+    }
+
+palpites.textContent += palpiteUsuario + " ";
+
+  if (palpiteUsuario === númeroAleatorio) {
+      ultimoResultado.textContent = "Parabéns! Você acertou!";
+      ultimoResultado.style.backgroundColor = "green";
+      baixoOuAlto.textContent = "";
+      finalizarJogo();
+  } else if (contagemPalpites === 10) {
+      ultimoResultado.textContent = "";
+      finalizarJogo();
+  } else {
+      ultimoResultado.textContent = "Errado";
+      ultimoResultado.style.backgroundColor = "red";
+      if (palpiteUsuario < numeroAleatorio) {
+          baixoOuAlto.textContent = "O último palpite foi muito baixo";
+      } else if (palpiteUsuario > numeroAleatorio() {
+          baixoOuAlto.textContent = "O último palpite foi muito alto";
+      }
+  }
